ba4f92a76a0a352799c558676b1cab57669f59b7
Pranit More <pranit.more@ritppune.com>
2026-07-16 14:34:52 +0530

    updated: fixed some bugs

--- src/generator/generate_answers.py
+++ src/generator/generate_answers.py
@@ -7,33 +7,37 @@ from retriever import retriever
 
 load_dotenv()
 
-API_KEY = os.getenv("API_KEY")
-
-chunks = retriever("What is perceptron?", top_k=3)
-
-context = " ".join([c["text"] for c in chunks])
-question = "What is perceptron?"
-
+API_KEY = os.getenv("GROQ_API_KEY")
 client = Groq(api_key=API_KEY)
 
-response = client.chat.completions.create(
-    model = "llama-3.3-70b-versatile",
-    messages= [
-        {
-            "role" : "system",
-            "content" : (
-                "Use only the provided context to answer the user's question. "
-                "If the answer is not in the context, say you don't know."
-                "Explain in simple, plain language, avoiding technical jargon and formulas where possible."
-                "give real life examples and explain everything if possible"
-            ),
-        },
-        {
-            "role" : "user",
-            "content" : f"Context:\n{context}\n\nQuestion:\n{question}",
-        },
-    ],
-)
 
-answer = response.choices[0].message.content
-print(answer)
+def generate_answer(question, top_k=3):
+    chunks = retriever(question, top_k=top_k)
+    context = " ".join([c["text"] for c in chunks])
+
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "system",
+                "content": (
+                    "Use only the provided context to answer the user's question. "
+                    "If the answer is not in the context, say you don't know. "
+                    "Explain in simple, plain language, avoiding technical jargon and formulas where possible. "
+                    "Give real life examples and explain everything if possible."
+                ),
+            },
+            {
+                "role": "user",
+                "content": f"Context:\n{context}\n\nQuestion:\n{question}",
+            },
+        ],
+    )
+
+    answer = response.choices[0].message.content
+    return answer, chunks
+
+
+if __name__ == "__main__":
+    answer, chunks = generate_answer("What is perceptron?", top_k=3)
+    print(answer)
